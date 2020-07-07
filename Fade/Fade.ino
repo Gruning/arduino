28a5b3567e33c3be55d01955ce228496e8a06f64
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-07-07 19:58:08 -0300

    Last test on Arduino Uno R3
    requirements
    
    resistor 220 ohm
    aligator hook
    led

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -15,7 +15,7 @@
 
 int led = 9;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int fadeAmount = 1;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -32,9 +32,10 @@ void loop() {
   brightness += fadeAmount;
 
   // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
+  if (brightness <= 0 || brightness >= 230) {
     fadeAmount = -fadeAmount;
+    delay(3000);
   }
   // wait for 30 milliseconds to see the dimming effect
-  delay(50);
+  delay(10);
 }
