b4f28e8aef048a4a96170c98991a3550f2c4ac87
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-24 19:13:29 -0300

    testing code

--- sec2cap15_literals/sec2cap15_literals.ino
+++ sec2cap15_literals/sec2cap15_literals.ino
@@ -2,10 +2,16 @@ int a = 42;
 char b ='G';
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13,HIGH);
+  
   Serial.print(a);
   Serial.println(b);
+  delay(1000);
+
+  digitalWrite(13,LOW);
   delay(1000);
 }
