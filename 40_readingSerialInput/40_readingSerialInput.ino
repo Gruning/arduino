fb0bc35d1229caccb212346cc8a92267652d04af
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-09 20:12:35 -0300

    sec2cap40_readingSerial

--- 40_readingSerialInput/40_readingSerialInput.ino
+++ 40_readingSerialInput/40_readingSerialInput.ino
@@ -1,8 +1,13 @@
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeOut(200);
 }
 
 void loop() {
   String myString = "";
+  while (myString!= ""){
+    myString =Serial.readString();
+  }
 
+Serial.println(myString)
 }
