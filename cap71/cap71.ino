48b5613ecd71682950958c01698ed665f9972669
Alberto Gruning <kimono-mac@Albertos-MacBook-Air.local>
2020-11-29 21:23:21 -0300

    cap71

---
+++ cap71/cap71.ino
@@ -0,0 +1,13 @@
+const led_1_pin = 3;
+const led_2_pin = 5;
+const led_3_pin = 6;
+
+void setup(){
+  pinMode(led_1_pin,OUTPUT);
+  pinMode(led_2_pin,OUTPUT);
+  pinMode(led_3_pin,OUTPUT);
+}
+
+void loop(){
+
+}
