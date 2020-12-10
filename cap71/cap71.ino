9a1fba870182dd26fda92ba3f434172e57d91730
gruning-macos <alberto.gruning.zen@gmail.com>
2020-12-09 21:12:52 -0300

    cap71-beforeDeleteAnalog

--- cap71/cap71.ino
+++ cap71/cap71.ino
@@ -6,6 +6,10 @@ void setup(){
   pinMode(led_1_pin,OUTPUT);
   pinMode(led_2_pin,OUTPUT);
   pinMode(led_3_pin,OUTPUT);
+
+  analogWrite(led_1_pin,5);
+  analogWrite(led_2_pin,20);
+  analogWrite(led_3_pin,255);
 }
 
 void loop(){
